d4bcb1d068737d3b52a7cc5e45b2d59fd3420116
Shorya Malani <github-sho@smlist.eml.cc>
2020-05-18 22:59:09 -0400

    adding image

--- static/script.js
+++ static/script.js
@@ -6,7 +6,7 @@ function submit_text() {
 
 
 socket.on("user_data_back", function(data) {
-    if (data["classes"].length() == 0) {
+    if (data["classes"].length == 0) {
         alert("You are not signed up for any classes yet.")
     }
 })
